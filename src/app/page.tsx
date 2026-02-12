4111abf58d317422d0dc95cef0b537a08bb717c5
saur0w <sthapliyal085@gmail.com>
2026-02-12 14:09:20 +0530

    "Page.tsx Structure fix"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import {use, useEffect} from "react";
+import { useEffect} from "react";
 import Lenis from '@studio-freight/lenis';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./page.module.css";
 import Landing from '@/components/Landing/index';
 import Parallax from '@/components/Parallax/index';
 import Footer from '@/components/Footer/index';
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
 
